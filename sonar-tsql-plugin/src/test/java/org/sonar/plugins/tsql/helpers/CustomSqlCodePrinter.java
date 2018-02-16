d5047439a4c7fe7f0f419944e38d0e88dfb59848
gretar <rd.greta@gmail.com>
2018-02-16 14:53:22 +0200

    Add helper

---
+++ sonar-tsql-plugin/src/test/java/org/sonar/plugins/tsql/helpers/CustomSqlCodePrinter.java
@@ -0,0 +1,43 @@
+package org.sonar.plugins.tsql.helpers;
+
+import org.sonar.plugins.tsql.checks.custom.Rule;
+import org.sonar.plugins.tsql.checks.custom.SqlRules;
+import org.sonar.plugins.tsql.rules.definitions.CustomPluginChecksProvider;
+
+public class CustomSqlCodePrinter {
+
+	public static void main(String[] args) {
+		final CustomPluginChecksProvider provider = new CustomPluginChecksProvider();
+		final SqlRules rules = provider.getRules();
+		System.out.println("# Rules #");
+		System.out.println();
+		System.out.println("Plugin supports the following rules:");
+		System.out.println();
+		for (Rule r : rules.getRule()) {
+			System.out.println(String.format("- %s - %s", r.getKey(), r.getName()));
+		}
+		System.out.println();
+		for (Rule r : rules.getRule()) {
+			System.out.println(String.format("## %s - %s ##", r.getKey(), r.getName()));
+			System.out.println(r.getDescription().replace("<h2>Description</h2>", ""));
+			System.out.println();
+			if (!r.getRuleImplementation().getCompliantRulesCodeExamples().getRuleCodeExample().isEmpty()) {
+				System.out.println("### Compliant examples ###");
+				System.out.println();
+				for (String c : r.getRuleImplementation().getCompliantRulesCodeExamples().getRuleCodeExample()) {
+					System.out.println(String.format("`%s`\r\n", c));
+				}
+			}
+
+			if (!r.getRuleImplementation().getViolatingRulesCodeExamples().getRuleCodeExample().isEmpty()) {
+				System.out.println("### Non-compliant examples ###");
+				System.out.println();
+				for (String c : r.getRuleImplementation().getViolatingRulesCodeExamples().getRuleCodeExample()) {
+					System.out.println(String.format("`%s`\r\n", c));
+				}
+			}
+		}
+
+	}
+
+}
